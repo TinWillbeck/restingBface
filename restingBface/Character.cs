edd154fad1db362dafe730d2a25c7e713ebf3fa2
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2024-01-19 12:46:04 +0100

    hbuio

---
+++ restingBface/Character.cs
@@ -0,0 +1,22 @@
+﻿public class Character
+{
+    public string name { get; set; }
+    public string mass { get; set; }
+    public string height { get; set; }
+    public string gender { get; set; }
+
+    public int WeightInt {
+        private set {}
+        get {
+            return int.Parse(mass);
+        }
+    }
+
+    public int weightInt;
+    public int heightInt;
+
+        // int.TryParse(height, out heightInt);
+//     public void getHeight()
+//     {
+//     }
+}
--- restingBface/Program.cs
+++ restingBface/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestSharp;
+using System.Text.Json;
+
+RestClient client = new("https://swapi.py4e.com/api/");
+
+int characterNumber = 3;
+RestRequest request = new($"people/{characterNumber}/");
+
+RestResponse response = client.GetAsync(request).Result;
+
+
+Character p = JsonSerializer.Deserialize<Character>(response.Content);
+
+
+Console.WriteLine(p.mass);
+// Console.WriteLine(response.Content);
+// File.WriteAllText("pokemon.json", response.Content);
+
+Console.ReadLine();
